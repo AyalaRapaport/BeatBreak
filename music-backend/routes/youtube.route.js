116d4971471cf5ce922743351eab4c33333f444f
AyalaRapaport <aarapaport@gmail.com>
2024-09-02 21:16:15 +0300

    fix

--- music-backend/routes/youtube.route.js
+++ music-backend/routes/youtube.route.js
@@ -1,9 +1,6 @@
 import express from 'express'
-import {downloadPlaylist, downloadYoutubeFile, searchVideos} from '../controllers/youtube.controller.js'
+import {searchVideos} from '../controllers/youtube.controller.js'
 const router = express.Router();
 router.get('/search', searchVideos);
-//router.post('/download', downloadPlaylist);
-router.post('/download', downloadYoutubeFile);
-
 
 export default router
