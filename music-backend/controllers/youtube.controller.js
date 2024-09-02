31a3a989e9ff40b805ed9077703068b2c9ece1eb
AyalaRapaport <aarapaport@gmail.com>
2024-09-02 21:19:31 +0300

    fix

--- music-backend/controllers/youtube.controller.js
+++ music-backend/controllers/youtube.controller.js
@@ -1,15 +1,6 @@
 import axios from 'axios';
-import youtubedl from 'youtube-dl-exec';
-import archiver from 'archiver';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import ytdl from 'ytdl-core';
-
 const API_KEY = process.env.YOUTUBE_API_KEY;
 const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export async function searchVideos(req, res) {
     const { query } = req.query;
